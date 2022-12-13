30f0ef20916e976c56d54016ca4f0dd8321c57ad
jonyolmeda <jonyolmeda@gmail.com>
2022-12-13 13:42:00 -0300

    Creación de controladores, rutas, modelo, schema e itemas en la base de datos

---
+++ routes/items.js
@@ -0,0 +1,17 @@
+let  router = require('express').Router()
+let {create, read, update, destroy, readId} = require('../controllers/item')
+const schema = require('../schemas/items')
+const validator = require('../middlewares/validator')
+const isTheSameUser = require('../middlewares/isTheSameUser')
+const Items = require('../models/Items')
+const passport = require ('../config/passport')
+/* const schemaItem = require('../schemas/itemEdit') */
+
+
+router.post('/',passport.authenticate("jwt", { session: false }), validator(schema), create)
+router.get('/', read)/* 
+router.put('/:id',passport.authenticate("jwt", { session: false }),validator(schemaItem), update) */
+router.delete('/:id',passport.authenticate("jwt", { session: false }),isTheSameUser(Items), destroy)
+router.get('/:id', readId)
+
+module.exports = router
