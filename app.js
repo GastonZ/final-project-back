e6f5b451ceb564c03142e998f81147449a3ee69c
GinoValenti <ginorvalenti@gmail.com>
2022-12-13 22:42:48 -0300

    fixed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors")
 require("dotenv").config()
 var createError = require('http-errors');
 var express = require('express');
@@ -5,7 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require("./config/database")
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -15,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
