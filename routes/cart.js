1d573e380f13451c0de9979b9efa4aa03c309b58
jonyolmeda <jonyolmeda@gmail.com>
2022-12-16 14:00:29 -0300

    Cart created

--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,11 @@
-/* const controllers = require("../controllers/");
+let  router = require('express').Router()
+let {addItemCart, deleteItem, getItems, getItemsCart, putItem} = require("../controllers/cart");
 
-router.get("/products", controllers.getProducts); //Obtiene prod de la DB
-router.get("/products-cart", controllers.getProductsCart); //Trae prod de la DB
-router.post("/products-cart", controllers.addProductCart); //Agrega prod al cart
-router.put("/products-cart/:productId", controllers.putProduct); //Modifica cantidad de prod en el cart
-router.delete("/products-cart/:productId", controllers.deleteProduct); //Elimina prod del cart
 
-module.exports = router */
+router.get("/items", getItems); //Obtiene prod de la DB
+router.get("/items-cart", getItemsCart); //Trae prod de la DB
+router.post("/items-cart", addItemCart); //Agrega prod al cart
+router.put("/items-cart/:itemId", putItem); //Modifica cantidad de prod en el cart
+router.delete("/items-cart/:itemId", deleteItem); //Elimina prod del cart
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const router = require('./users');
 let users = require('./users')
 let cars = require('./cars')
 let items = require('./items')
-/* let cart = require('./cart')
- */
+let cart = require('./cart')
+
 router.use('/cars', cars)
 router.use('/auth', users)
 router.use('/items', items)
-/* router.use('/cart', cart)
- */
+router.use('/cart', cart)
+
 
 
 module.exports = router;
