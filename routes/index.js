23cb039e84130a881d1e218f04014d49756c6262
GastonZ <gastonzappullaryu@gmail.com>
2022-12-21 18:21:14 -0300

    crear testimonios

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ let users = require('./users')
 let cars = require('./cars')
 let items = require('./items')
 let cart = require('./cart')
+let comments = require('./comments')
 
 router.use('/cars', cars)
 router.use('/auth', users)
 router.use('/items', items)
 router.use('/cart', cart)
+router.use('/comments', comments)
 
 
 
